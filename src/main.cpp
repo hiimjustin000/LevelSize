1ac4ed9af3bbd600d8f15eeb11b54277c102add2
Justin Pridgen <justincpridgen672@gmail.com>
2024-05-18 16:17:32 -0400

    v1.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
+#include <numeric>
 #include <Geode/Geode.hpp>
-#include <Geode/modify/LevelBrowserLayer.hpp>
-#include <Geode/modify/LevelCell.hpp>
 
 using namespace geode::prelude;
 
 bool sortBySize = false;
 
 std::string roundedDivision(int a, int b) {
-    std::string str = std::to_string(std::round(a * 100.0f / b) / 100.0f);
-    return str.find('.') != std::string::npos && str.substr(str.find('.')).size() > 3 ? str.substr(0, str.find('.') + 3) : str;
+    auto str = std::to_string(std::round(a * 100.0f / b) / 100.0f);
+    auto found = str.find('.');
+    return found != std::string::npos && str.substr(found).size() > 3 ? str.substr(0, found + 3) : str;
 }
 
 std::string getSizeString(int size) {
@@ -18,88 +18,74 @@ std::string getSizeString(int size) {
     else return roundedDivision(size, 1073741824) + " GB";
 }
 
-bool compareLevels(GJGameLevel* a, GJGameLevel* b) {
-    return a->m_levelString.size() > b->m_levelString.size();
+int getTotalSize(CCArray* levelArray) {
+    auto levels = CCArrayExt<GJGameLevel*>(levelArray);
+    return std::accumulate(levels.begin(), levels.end(), 0, [](auto a, auto x) { return a + x->m_levelString.size(); });
 }
 
-int getTotalSize(CCArray* levels) {
-    int totalSize = 0;
-    CCObject* obj;
-    CCARRAY_FOREACH(levels, obj) {
-        totalSize += static_cast<GJGameLevel*>(obj)->m_levelString.size();
-    }
-    return totalSize;
-}
-
+#include <Geode/modify/LevelBrowserLayer.hpp>
 class $modify(LSLevelBrowserLayer, LevelBrowserLayer) {
     bool init(GJSearchObject* searchObject) {
         if (!LevelBrowserLayer::init(searchObject)) return false;
-        CCSize winSize = CCDirector::sharedDirector()->getWinSize();
 
         if (searchObject->m_searchType == SearchType::MyLevels) {
-            CCMenu* myLevelsMenu = static_cast<CCMenu*>(this->getChildByID("my-levels-menu"));
-            CCMenu* sizeSortMenu = CCMenu::create();
-            sizeSortMenu->setPosition(0.0f, 0.0f);
+            auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+            auto sizeSortMenu = CCMenu::create();
+            sizeSortMenu->setPosition(winSize.width / 2, winSize.height / 2 - 124.0f);
             sizeSortMenu->setID("size-sort-menu"_spr);
-            CCMenuItemToggler* sizeSortToggler = CCMenuItemToggler::create(
-                CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png"),
-                CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png"),
-                this,
-                menu_selector(LSLevelBrowserLayer::onSortBySize)
-            );
+            addChild(sizeSortMenu, 1);
+            
+            auto spriteOff = ButtonSprite::create(CCSprite::create("LS_toggleBtn_001.png"_spr), 32, true, 32.0f, "GJ_button_01.png", 1.0f);
+            spriteOff->setScale(0.5f);
+            auto spriteOn = ButtonSprite::create(CCSprite::create("LS_toggleBtn_001.png"_spr), 32, true, 32.0f, "GJ_button_02.png", 1.0f);
+            spriteOn->setScale(0.5f);
+            auto sizeSortToggler = CCMenuItemToggler::create(spriteOff, spriteOn, this, menu_selector(LSLevelBrowserLayer::onSortBySize));
             sizeSortToggler->toggle(sortBySize);
-            sizeSortToggler->setPosition(myLevelsMenu->getPositionX(), myLevelsMenu->getPositionY() + myLevelsMenu->getScaledContentSize().height / 2 - 60.0f);
             sizeSortToggler->setID("size-sort-toggler"_spr);
-            CCLabelBMFont* sizeSortLabel = CCLabelBMFont::create("Sort by\nSize", "bigFont.fnt", kCCLabelAutomaticWidth, kCCTextAlignmentCenter);
-            sizeSortLabel->setAnchorPoint({ 0.5f, 0.0f });
-            sizeSortLabel->setScale(0.4f);
-            sizeSortLabel->setPosition(sizeSortToggler->getPositionX(), sizeSortToggler->getPositionY() + sizeSortToggler->getScaledContentSize().height / 2 + 5.0f);
-            sizeSortLabel->setID("size-sort-label"_spr);
             sizeSortMenu->addChild(sizeSortToggler);
-            this->addChild(sizeSortMenu);
-            this->addChild(sizeSortLabel);
+
             if (Mod::get()->getSettingValue<bool>("show-total-size")) {
-                CCLabelBMFont* totalSizeLabel = CCLabelBMFont::create(("Total Size: " + getSizeString(getTotalSize(m_list->m_listView->m_entries))).c_str(), "bigFont.fnt");
+                auto totalSizeLabel = CCLabelBMFont::create(fmt::format("Total Size: {}", getSizeString(getTotalSize(m_list->m_listView->m_entries))).c_str(), "bigFont.fnt");
                 totalSizeLabel->setScale(0.4f);
-                if (totalSizeLabel->getScaledContentSize().width > 130.0f) totalSizeLabel->setScale(52.0f / totalSizeLabel->getScaledContentSize().width);
+                totalSizeLabel->limitLabelWidth(130.0f, 0.4f, 0.0f);
                 totalSizeLabel->setPosition(winSize.width / 2 + 95.0f, winSize.height / 2 - 122.0f);
                 totalSizeLabel->setID("total-size-label"_spr);
-                this->addChild(totalSizeLabel, 10);
+                addChild(totalSizeLabel, 10);
             }
         }
 
         return true;
     }
 
-    void sortLevelsBySize(bool enabled) {
-        CCObject* obj;
-        LocalLevelManager* llm = LocalLevelManager::sharedState();
-        std::vector<GJGameLevel*> levels;
-        CCARRAY_FOREACH(llm->m_localLevels, obj) {
-            levels.push_back(static_cast<GJGameLevel*>(obj));
+    void sortLevelsBySize() {
+        auto llm = LocalLevelManager::sharedState();
+        auto levels = std::vector<GJGameLevel*>();
+        for (auto level : CCArrayExt<GJGameLevel*>(llm->m_localLevels)) {
+            levels.push_back(level);
         }
-        if (enabled) std::sort(levels.begin(), levels.end(), compareLevels);
-        CCArray* newArr = CCArray::create();
+        if (sortBySize) std::sort(levels.begin(), levels.end(), [](auto a, auto b) { return a->m_levelString.size() > b->m_levelString.size(); });
+        auto newArr = CCArray::create();
         for (int i = m_pageStartIdx; i < m_pageStartIdx + m_pageEndIdx && i < levels.size(); i++) {
             newArr->addObject(levels[i]);
         }
         LevelBrowserLayer::setupLevelBrowser(newArr);
     }
 
-    void onSortBySize(CCObject* sender) {
+    void onSortBySize(CCObject*) {
         sortBySize = !sortBySize;
-        sortLevelsBySize(sortBySize);
+        sortLevelsBySize();
     }
 
     void setupLevelBrowser(CCArray* levelArray) {
         if (m_searchObject->m_searchType == SearchType::MyLevels) {
-            if (sortBySize) sortLevelsBySize(true);
+            if (sortBySize) sortLevelsBySize();
             else LevelBrowserLayer::setupLevelBrowser(levelArray);
             if (Mod::get()->getSettingValue<bool>("show-total-size")) {
-                CCLabelBMFont* totalSizeLabel = static_cast<CCLabelBMFont*>(this->getChildByID("total-size-label"_spr));
+                auto totalSizeLabel = static_cast<CCLabelBMFont*>(getChildByID("total-size-label"_spr));
                 if (totalSizeLabel != nullptr) {
-                    totalSizeLabel->setString(("Total Size: " + getSizeString(getTotalSize(m_list->m_listView->m_entries))).c_str());
-                    if (totalSizeLabel->getScaledContentSize().width > 130.0f) totalSizeLabel->setScale(52.0f / totalSizeLabel->getScaledContentSize().width);
+                    totalSizeLabel->setString(fmt::format("Total Size: {}", getSizeString(getTotalSize(m_list->m_listView->m_entries))).c_str());
+                    totalSizeLabel->limitLabelWidth(130.0f, 0.4f, 0.0f);
                 }
             }
         }
@@ -107,12 +93,13 @@ class $modify(LSLevelBrowserLayer, LevelBrowserLayer) {
     }
 };
 
+#include <Geode/modify/LevelCell.hpp>
 class $modify(LSLevelCell, LevelCell) {
     void loadLocalLevelCell() {
         LevelCell::loadLocalLevelCell();
 
         if (Mod::get()->getSettingValue<bool>("show-size")) {
-            CCLabelBMFont* sizeLabel = CCLabelBMFont::create(getSizeString(m_level->m_levelString.size()).c_str(), "goldFont.fnt");
+            auto sizeLabel = CCLabelBMFont::create(getSizeString(m_level->m_levelString.size()).c_str(), "goldFont.fnt");
             sizeLabel->setPosition(m_width - 6.0f, 3.0f);
             sizeLabel->setAnchorPoint({ 1.0f, 0.0f });
             sizeLabel->setScale(0.4f);
